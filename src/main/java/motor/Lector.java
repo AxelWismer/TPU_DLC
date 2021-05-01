ef008f61d08ffb6f6aec7abbad563518d002b034
AxelWismer <mmowismer@outlook.com>
2021-05-01 16:53:16 -0300

    Mejora lector con codigo visto en clase

--- src/main/java/motor/Lector.java
+++ src/main/java/motor/Lector.java
@@ -1,30 +1,17 @@
 package motor;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Lector {
-    //Guarda todas las palabras que se encuentran en un documento
-    //    public Hashtable<String, Palabra> vocabulario = new Hashtable<String, Palabra>();
-    protected Hashtable<String, Integer> vocabulario = new Hashtable<>();
-
-    public void agregarPalabra(String palabra){
-        // Si el vocabulario ya contiene la palabra aumentar el tf en 1
-        if (vocabulario.containsKey(palabra)){
-            vocabulario.put(palabra, vocabulario.get(palabra) + 1);
-        }
-        else{
-            // Sino crear la entrada con tf 1
-            vocabulario.put(palabra, 1);
-        }
-    }
-
     public Hashtable<String,Integer> armarVocabulario(String carpeta, String documento){
         //Abrir el documento seleccionado
         Scanner scanner;
         try {
-            scanner = new Scanner(new File(carpeta +  "/" + documento), "Cp1252");
+            scanner = new Scanner(new BufferedReader(new FileReader(carpeta +  "/" + documento)));
+            scanner.useDelimiter("[\\W\\s]");
         }
         catch (FileNotFoundException e) {
             System.out.println("Archivo no encontrado");
@@ -32,28 +19,17 @@ public class Lector {
         }
         //Iterar el archivo obteniendo palabras
         String palabra;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            int inicio = -1;
-
-            for (int i = 0; i < line.length(); i++) {
-                //Revisa que no se este analizando una palabra y que el caracter por el que se pasa sea una letra
-                if (inicio == -1 && Character.isLetter(line.charAt(i))) {
-                    inicio = i;
+        Hashtable<String, Integer> vocabulario = new Hashtable<>();
+        while (scanner.hasNext()) {
+            palabra = scanner.next();
+            if (!palabra.isBlank()) {
+                if (vocabulario.containsKey(palabra)){
+                    vocabulario.put(palabra, vocabulario.get(palabra) + 1);
                 }
-                else {
-                    //Si ya se encuentra iterando una palbra
-                    if (inicio != -1 && !Character.isLetter(line.charAt(i)) && line.charAt(i) != '\'' ){
-                        palabra = line.substring(inicio, i);
-                        //System.out.println(palabra);
-                        agregarPalabra(palabra.toLowerCase());
-                        inicio = -1;
-                    }
+                else{
+                    // Sino crear la entrada con tf 1
+                    vocabulario.put(palabra, 1);
                 }
-            }
-            if (inicio != -1) {
-                palabra = line.substring(inicio);
-                agregarPalabra(palabra.toLowerCase());
             }
         }
         return vocabulario;
--- src/test/java/motor/LectorTest.java
+++ src/test/java/motor/LectorTest.java
@@ -15,28 +15,15 @@ public class LectorTest {
          System.out.println("* UtilsJUnit4Test: @Before method");
          instance = new Lector();
     }
-    @org.junit.Test
-    public void TestLector() {
-        instance.agregarPalabra("palabra1");
-        instance.agregarPalabra("palabra2");
-        instance.agregarPalabra("palabra1");
-
-        assertEquals(2, instance.vocabulario.size());
-        assertEquals(Integer.valueOf(2), instance.vocabulario.get("palabra1"));
-        assertEquals(Integer.valueOf(1), instance.vocabulario.get("palabra2"));
-    }
 
     @org.junit.Test
     public void TestLectorArmarVocabulario() {
         //Crea una tabla para todos los documentos
         Controlador controlador = new Controlador();
         String[] documentos = controlador.getDocumentosEnCarpeta("DocumentosTP1");
-        ArrayList<Hashtable<String, Integer>> vocabulario = new ArrayList<>();
         for (String documento : documentos) {
-            vocabulario.add(instance.armarVocabulario("DocumentosTP1", documento));
+            instance.armarVocabulario("DocumentosTP1", documento);
         }
-        assertEquals(593, vocabulario.size());
         //assertEquals(13473, instance.vocabulario.size());
     }
-
 }
